7e685e30c47cbef93893290e88b332b01d551c88
Diya Betcy <diyabetcy123@gmail.com>
2025-07-07 14:02:40 +0530

    add product by admin ui

--- lib/screens/admin/dashboardAdmin.dart
+++ lib/screens/admin/dashboardAdmin.dart
@@ -36,7 +36,80 @@ class _AdminProductPageState extends State<AdminProductPage> {
   }
 
   Future<void> addProduct() async {
-    
+    final _formKey = GlobalKey<FormState>();
+    String title = "", description='', category = '', thumbnail='';
+    int price = 0;
+
+    await showDialog(
+      context: context, 
+      builder: (context)=> AlertDialog(
+        title: Text('Add New product'),
+        content: SingleChildScrollView(
+          child: Form( 
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  onChanged: (val) => title =  val,
+                  validator: (val) => val!.isEmpty? 'Enter title': null,
+                ),
+                 TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  keyboardType: TextInputType.number,
+                  onChanged: (val) => price = int.tryParse(val) ?? 0,
+                  validator: (val) => val!.isEmpty ? 'Enter price' : null,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Description'),
+                  onChanged: (val) => description = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Category'),
+                  onChanged: (val) => category = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Thumbnail URL'),
+                  onChanged: (val) => thumbnail = val,
+                  enableInteractiveSelection: true,
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(onPressed: ()=> Navigator.pop(context), child: Text('Cancel')),
+          ElevatedButton(
+            onPressed: () async{
+              if(_formKey.currentState!.validate()){
+                final response = await http.post(
+                  Uri.parse('https://dummyjson.com/products/add'),
+                  headers: {'Content-Type': 'application/json'},
+                  body: jsonEncode({
+                    'title': title,
+                    'price': price,
+                    'description': description,
+                    'category': category,
+                    'thumbnail': thumbnail,
+                  }),
+                );
+                if (response.statusCode == 200 || response.statusCode == 201){
+                  final data = jsonDecode(response.body);
+                  setState(() => products.insert(0,data));
+                  Navigator.pop(context);
+                } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Failed to add product')),
+                );
+              }
+              }
+            }, 
+            child: Text('Add')
+          )
+        ],
+      )
+    );
   }
 
   Future<void> updateProduct(int id) async {
